961935d0f7a1af065be32fe6f632efd662a892b0
Tiago Farias <tiago.farias.poa@gmail.com>
2024-05-01 22:35:21 -0300

    Add - faker: PT_BR

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
     /** Register any application services. */
     public function register(): void
     {
-
+        $this->app->singleton(\Faker\Generator::class, function () {
+            return \Faker\Factory::create('pt_BR');
+        });
     }
 
     /** Bootstrap any application services. */

728d6bd2e9cf051e368e66736437bd21472a3c12
Tiago Farias <tiago.farias.poa@gmail.com>
2024-05-02 15:56:40 -0300

    Add - config: authorization service

---
+++ config/authorization.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'base_url' => env('AUTHORIZATION_BASE_URL', 'https://run.mocky.io'),
+    'endpoints' => [
+        'get_authorization' => env('AUTHORIZATION_URL',"/v3/5794d450-d2e2-4412-8131-73d0293ac1cc"),
+    ],
+];

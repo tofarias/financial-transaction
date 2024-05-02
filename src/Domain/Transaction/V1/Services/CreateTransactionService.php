8e182b3a7bdc844b698e732859e4ae12cade0c7f
Tiago Farias <tiago.farias.poa@gmail.com>
2024-05-02 14:49:15 -0300

    Add - transaction service: db transaction

--- src/Domain/Transaction/V1/Services/CreateTransactionService.php
+++ src/Domain/Transaction/V1/Services/CreateTransactionService.php
@@ -8,12 +8,14 @@ use Domain\Users\V1\Services\UserQuery;
 use Domain\Shared\Services\BaseServiceExecute;
 use Domain\Transaction\V1\Exceptions\TransactionException;
 use Domain\Wallets\V1\Services\WalletCommand;
+use Illuminate\Support\Facades\DB;
 
 class CreateTransactionService extends BaseServiceExecute
 {
     public function execute(): mixed
     {
         $dtoValidData = $this->dto->valid_data;
+
         $payer = UserQuery::findById($dtoValidData->payer);
         $payee = UserQuery::findById($dtoValidData->payee);
 
@@ -21,14 +23,16 @@ class CreateTransactionService extends BaseServiceExecute
             throw new TransactionException('Payer cannot be a shopkeeper');
         }
 
-        $newTransaction = TransactionCommand::create(
-            $payer->wallet,
-            $payee->wallet,
-            $dtoValidData->value
-        );
-        WalletCommand::debit($payer->wallet, $newTransaction->value);
-        WalletCommand::credit($payee->wallet, $newTransaction->value);
+        return DB::transaction(function () use ($payer, $payee, $dtoValidData) {
+            $newTransaction = TransactionCommand::create(
+                $payer->wallet,
+                $payee->wallet,
+                $dtoValidData->value
+            );
+            WalletCommand::debit($payer->wallet, $newTransaction->value);
+            WalletCommand::credit($payee->wallet, $newTransaction->value);
 
-        return $newTransaction;
+            return $newTransaction;
+        });
     }
 }

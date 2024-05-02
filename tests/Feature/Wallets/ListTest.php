ddcdfb1812e95591168fb76abdf15d72316788a5
Tiago Farias <tiago.farias.poa@gmail.com>
2024-05-02 20:07:35 -0300

    Add - test: list wallets

--- tests/Feature/Wallets/ListTest.php
+++ tests/Feature/Wallets/ListTest.php
@@ -13,18 +13,22 @@ beforeEach(function () {
 it('should return 200', function () {
 
     getJson(
-        route('v1:users.index')
+        route('v1:wallets.index')
     )
         ->assertOk()
         ->assertJsonStructure([
             '*' => [
-                'id',
-                'name',
-                'email',
-                'type',
-                'doc_type',
-                'doc_number',
-                'timezone',
+                "id",
+                "balance",
+                "user" => [
+                    "id",
+                    "name",
+                    "email",
+                    "type",
+                    "doc_type",
+                    "doc_number",
+                    "timezone"
+                ]
             ],
         ]);
 });

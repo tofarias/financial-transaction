840caa747f81c3c409fa78a5ed5d8d48325a6854
Tiago Farias <tiago.farias.poa@gmail.com>
2024-05-02 14:42:32 -0300

    Add - transaction service

---
+++ src/Domain/Transaction/V1/Services/CreateTransactionService.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Transaction\V1\Services;
+
+use Domain\Users\V1\Services\UserQuery;
+use Domain\Shared\Services\BaseServiceExecute;
+use Domain\Transaction\V1\Exceptions\TransactionException;
+use Domain\Wallets\V1\Services\WalletCommand;
+
+class CreateTransactionService extends BaseServiceExecute
+{
+    public function execute(): mixed
+    {
+        $dtoValidData = $this->dto->valid_data;
+        $payer = UserQuery::findById($dtoValidData->payer);
+        $payee = UserQuery::findById($dtoValidData->payee);
+
+        if($payer->isShopkeeper()) {
+            throw new TransactionException('Payer cannot be a shopkeeper');
+        }
+
+        $newTransaction = TransactionCommand::create(
+            $payer->wallet,
+            $payee->wallet,
+            $dtoValidData->value
+        );
+        WalletCommand::debit($payer->wallet, $newTransaction->value);
+        WalletCommand::credit($payee->wallet, $newTransaction->value);
+
+        return $newTransaction;
+    }
+}

39caec4dab5b181df080926db5459c855536abe2
Tiago Farias <tiago.farias.poa@gmail.com>
2024-05-02 15:57:10 -0300

    Add - UnauthorizedTransactionException

---
+++ src/Domain/Integrations/V1/Api/Authorization/Exceptions/UnauthorizedTransactionException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Integrations\V1\Api\Authorization\Exceptions;
+
+use DomainException;
+use Exception;
+
+class UnauthorizedTransactionException extends DomainException
+{
+    public function __construct(string $message = '', int $code = 422, ?Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}

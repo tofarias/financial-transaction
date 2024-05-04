9e0c87137e405021f5f2d1d2f4873415f941f772
Tiago Farias <tiago.farias.poa@gmail.com>
2024-05-04 13:48:30 -0300

    Add - service provider: bind classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Domain\Integrations\V1\Services\RabbitMQ\Publisher;
+use Domain\Notification\V1\NotificationPublisher;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -15,6 +17,12 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(\Faker\Generator::class, function () {
             return \Faker\Factory::create('pt_BR');
+        });
+
+        $this->app->bind(NotificationPublisher::class, function ($app) {
+            $publicher = new Publisher('A New transaction has been created successfully!');
+
+            return new NotificationPublisher($publicher);
         });
     }
 
